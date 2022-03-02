8367a789dceb7ce84c651aa2e079f1fbadf02a51
Korchak Konstantin <korchak.konstantin@gmail.com>
2022-03-02 23:37:59 +0300

    logic development

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redColor: UIView!
+    @IBOutlet weak var yellowColor: UIView!
+    @IBOutlet weak var greenColor: UIView!
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let viewWidth = redColor.bounds.width
+        redColor.layer.cornerRadius = viewWidth / 2
+        yellowColor.layer.cornerRadius = viewWidth / 2
+        greenColor.layer.cornerRadius = viewWidth / 2
+        redColor.alpha = 0.3
+        yellowColor.alpha = 0.3
+        greenColor.alpha = 0.3
+        button.layer.cornerRadius = 10
     }
-
-
+    
+    @IBAction func changeColorButtonPress() {
+        if yellowColor.alpha + greenColor.alpha + redColor.alpha < 1 {
+            button.setTitle("NEXT", for: .normal)
+            redColor.alpha = 1
+        } else if redColor.alpha >= 1 {
+            redColor.alpha = 0.3
+            yellowColor.alpha = 1
+        } else if yellowColor.alpha >= 1 {
+            yellowColor.alpha = 0.3
+            greenColor.alpha = 1
+        } else {
+            greenColor.alpha = 0.3
+            redColor.alpha = 1
+        }
+    }
 }
 
