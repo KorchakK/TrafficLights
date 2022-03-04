ca1483fb3cdd43a2f52123127b5a1d1300f822dd
Korchak Konstantin <korchak.konstantin@gmail.com>
2022-03-04 17:19:29 +0300

    final build logic and sb

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -7,38 +7,55 @@
 
 import UIKit
 
+enum CurrentColor {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var redColor: UIView!
     @IBOutlet weak var yellowColor: UIView!
     @IBOutlet weak var greenColor: UIView!
+    
     @IBOutlet weak var button: UIButton!
+    
+    private var currentColor = CurrentColor.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let viewWidth = redColor.bounds.width
-        redColor.layer.cornerRadius = viewWidth / 2
-        yellowColor.layer.cornerRadius = viewWidth / 2
-        greenColor.layer.cornerRadius = viewWidth / 2
-        redColor.alpha = 0.3
-        yellowColor.alpha = 0.3
-        greenColor.alpha = 0.3
         button.layer.cornerRadius = 10
+        
+        redColor.alpha = lightIsOff
+        yellowColor.alpha = lightIsOff
+        greenColor.alpha = lightIsOff
+    }
+    
+    override func viewDidLayoutSubviews() {
+        greenColor.layer.cornerRadius = greenColor.frame.width / 2
+        yellowColor.layer.cornerRadius = greenColor.frame.width / 2
+        redColor.layer.cornerRadius = greenColor.frame.width / 2
     }
     
     @IBAction func changeColorButtonPress() {
-        if yellowColor.alpha + greenColor.alpha + redColor.alpha < 1 {
+        if button.currentTitle == "START" {
             button.setTitle("NEXT", for: .normal)
-            redColor.alpha = 1
-        } else if redColor.alpha >= 1 {
-            redColor.alpha = 0.3
-            yellowColor.alpha = 1
-        } else if yellowColor.alpha >= 1 {
-            yellowColor.alpha = 0.3
-            greenColor.alpha = 1
-        } else {
-            greenColor.alpha = 0.3
-            redColor.alpha = 1
+        }
+        
+        switch currentColor {
+        case .red:
+            redColor.alpha = lightIsOn
+            greenColor.alpha = lightIsOff
+            currentColor = .yellow
+        case .yellow:
+            yellowColor.alpha = lightIsOn
+            redColor.alpha = lightIsOff
+            currentColor = .green
+        case .green:
+            greenColor.alpha = lightIsOn
+            yellowColor.alpha = lightIsOff
+            currentColor = .red
         }
     }
 }
